6859bb9fd85ce047875366149e3f6b3e5bd1dc3e
Kenaz <66073275+Kencel@users.noreply.github.com>
2026-06-06 16:22:22 +0800

    integrate ostree into default template

--- templates/c++/maxflow.cpp
+++ templates/c++/maxflow.cpp
@@ -1,6 +1,12 @@
 #include <bits/stdc++.h>
 
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+ 
+using namespace __gnu_pbds;
 using namespace std;
+ 
+typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update> ordered_multiset;
 
 #define ll long long
 #define vll vector<ll>
